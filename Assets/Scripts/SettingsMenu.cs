cc2f67d442846ede21627919137e833e2d7e5571
CestVraimentTropDrole <149939786+CestVraimentTropDrole@users.noreply.github.com>
2026-01-07 12:58:13 +0100

    feat: start of main menu

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+
+    public void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+}
