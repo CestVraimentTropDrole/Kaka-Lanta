996057b2f16827485588d07d3b204bc5a7afe4df
CestVraimentTropDrole <149939786+CestVraimentTropDrole@users.noreply.github.com>
2026-01-09 09:15:21 +0100

    feat: test linux export & dropdown menu

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -1,17 +1,29 @@
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using TMPro;
+using System.Collections.Generic;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
-    public Dropdown resolutionDropdown;
+    public TMP_Dropdown resolutionDropdown;
     Resolution[] resolutions;
 
     public void Start()
     {
         resolutions = Screen.resolutions;
         resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+        }
+
+        resolutionDropdown.AddOptions(options);
     }
 
     public void SetVolume(float volume)
