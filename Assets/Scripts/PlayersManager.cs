e0dbe1e9f0a6c3c90f12a3ea2dd9a44a35886d13
CestVraimentTropDrole <149939786+CestVraimentTropDrole@users.noreply.github.com>
2026-01-13 14:22:30 +0100

    feat: creating multiple players at start

--- Assets/Scripts/PlayersManager.cs
+++ Assets/Scripts/PlayersManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class PlayersManager : MonoBehaviour
 {
@@ -6,10 +7,26 @@ public class PlayersManager : MonoBehaviour
     public Transform spawnPoints;
     public int numberPlayers;
 
+    public Color[] playerColors = new Color[] 
+    { 
+        Color.red,      // Joueur 1
+        Color.blue,     // Joueur 2
+        Color.green,    // Joueur 3
+        Color.yellow    // Joueur 4
+    };
+
     public void Start()
     {
-        GameObject player = Instantiate(playerPrefab, spawnPoints.position, Quaternion.identity);
-        player.name = "Player " + 1;
+        for (int i = 0; i < numberPlayers; i++)
+        {
+            GameObject player = Instantiate(playerPrefab, spawnPoints.position, Quaternion.identity);
+            player.name = "Player " + (i + 1);
+
+            SpriteRenderer sprite = player.GetComponent<SpriteRenderer>();
+            sprite.color = playerColors[i];
+
+            Debug.Log("Joueur " + (i+1) + " créé avec succès en " + spawnPoints.position);
+        }
     }
 
     
